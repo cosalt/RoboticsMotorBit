1e47dd88d4ab6e355a996b144bc41f60f456935a
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-10-28 15:13:04 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -29,7 +29,7 @@ basic.forever(function () {
     if (Distance <= 10 && Distance != 0) {
         // Move forward
         motorbit.freestyle(motor_speed, 0)
-        basic.pause(550)
+        basic.pause(525)
         // Move forward
         motorbit.freestyle(0, 0)
         basic.pause(Pause_time)
@@ -41,7 +41,7 @@ basic.forever(function () {
         basic.pause(Pause_time)
         // Move forward
         motorbit.freestyle(0, motor_speed)
-        basic.pause(550)
+        basic.pause(525)
         // Move forward
         motorbit.freestyle(0, 0)
         basic.pause(Pause_time)
@@ -53,7 +53,7 @@ basic.forever(function () {
         basic.pause(Pause_time)
         // Move forward
         motorbit.freestyle(0, motor_speed)
-        basic.pause(550)
+        basic.pause(525)
         // Move forward
         motorbit.freestyle(0, 0)
         basic.pause(Pause_time)
@@ -65,7 +65,7 @@ basic.forever(function () {
         basic.pause(Pause_time)
         // Move forward
         motorbit.freestyle(motor_speed, 0)
-        basic.pause(550)
+        basic.pause(525)
         // Move forward
         motorbit.freestyle(0, 0)
         basic.pause(Pause_time)
